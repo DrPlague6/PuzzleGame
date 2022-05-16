f70a39c8cdb2d2a507af390d4c9a8bf64fec3f58
DrPlague6 <drplague@tutanota.com>
2022-05-16 15:30:35 +0200

    PlateAnimation

--- Assets/Scripts/Triggers/PlateTrigger.cs
+++ Assets/Scripts/Triggers/PlateTrigger.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class PlateTrigger : MonoBehaviour
 {
     [SerializeField] GameEvent _triggerDoors;
+    [SerializeField] Animation plateAniamtion;
+    HashSet<int> triggersIn = new HashSet<int>();
     void OnTriggerEnter(Collider other){
-        Debug.Log("Collided");      
-        Debug.Log(other.gameObject.tag);
-        Debug.Log(other.gameObject.name);
-        if(other.gameObject.tag != "Player" && other.gameObject.tag != "Pushable"){
-            Debug.Log("Not Player");
+        CheckEnter(other);
+    }
+    void OnTriggerExit(Collider other){
+       CheckExit(other);
+    }
+    void OnCollisionEnter(Collision other){
+        CheckEnter(other.collider);
+    }
+    void OnCollisionExit(Collision other){
+        CheckEnter(other.collider);
+    }
+    private void CheckEnter(Collider other){
+        if(other.gameObject.tag != "Player" && other.gameObject.tag != "Pushable")
             return;
-        }
         _triggerDoors.Invoke();
+        triggersIn.Add(other.gameObject.GetHashCode());
+        if(triggersIn.Count > 1)
+            return;
+        plateAniamtion["PlatePushed"].speed = 1;
+        plateAniamtion.Play();
+    }
+    private void CheckExit(Collider other){
+        triggersIn.Remove(other.gameObject.GetInstanceID());
+        if(triggersIn.Count != 0)
+        return;
+        plateAniamtion["PlatePushed"].speed = -1;
+        plateAniamtion.Play();    
     }
 }
